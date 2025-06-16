96cd49dc57d9b4546ab982b739b7f3a24ad1cf3d
Advait Maindalkar <advait@incubyte.co>
2025-06-16 19:39:14 +0530

    failing test case for single number

--- tests/string-calculator.test.js
+++ tests/string-calculator.test.js
@@ -9,4 +9,8 @@ describe("String Calculator", () => {
   it('should return 0 for an empty string', () => {
     expect(stringCalculator('')).toBe(0);
   })
+
+  it('should return the same number for a single number', () => {
+    expect(stringCalculator('1')).toBe(1);
+  })
 });
