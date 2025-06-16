aa6443c422abb0d1637a886ce86815054c9d68b7
Advait Maindalkar <advait@incubyte.co>
2025-06-16 19:32:30 +0530

    added initial test to fail - stringCalculator doesn't exist

---
+++ tests/string-calculator.test.js
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+import { stringCalculator } from "../src/string-calculator";
+
+describe("String Calculator", () => {
+  it('should be defined', () => {
+    expect(stringCalculator).toBeDefined();
+  })
+});
